778dd5bab24c76a6a30444e0c40147cdc998a4bb
Permia Community <35648833+sakaturu@users.noreply.github.com>
2026-02-08 20:52:34 -0600

    Update sampleData.ts

--- services/sampleData.ts
+++ services/sampleData.ts
@@ -40,7 +40,7 @@ const INITIAL_VIDEO_DATA: any[] = [
   },
   {
     "id": "v-fav-001",
-    "prompt": "Heal The Earth - 432Hz Healing Frequency",
+    "prompt": "Heal The Earth",
     "category": "Fav. Pick",
     "url": "_5aA54MD4ho",
     "thumbnail": "https://img.youtube.com/vi/_5aA54MD4ho/mqdefault.jpg"
